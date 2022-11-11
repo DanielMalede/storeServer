9639c9d1dac657b3dd8982afc03c3182b6654596
DanielMalede <danielmalede@gmail.com>
2022-11-11 10:57:59 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const { log } = require("console");
+const app = express();
+const port = 5000;
+
+const product = [
+  {
+    id: 1,
+    productName: "Phone",
+    description: "brand new",
+    price: 1500,
+    quantity: 4,
+    img: "someUrl",
+  },
+  {
+    id: 2,
+    productName: "Tv",
+    description: "brand new",
+    price: 2000,
+    quantity: 10,
+    img: "someUrl",
+  },
+  {
+    id: 3,
+    productName: "Car",
+    description: "brand new",
+    price: 40000,
+    quantity: 15,
+    img: "someUrl",
+  },
+  {
+    id: 4,
+    productName: "watch",
+    description: "brand new",
+    price: 500,
+    quantity: 7,
+    img: "someUrl",
+  },
+  {
+    id: 5,
+    productName: "shoes",
+    description: "brand new",
+    price: 700,
+    quantity: 20,
+    img: "someUrl",
+  },
+];
+
+app.use(cors());
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/store", (req, res) => {
+  res.send("server is good");
+});
+
+app.get('/store/product',(req,res)=>{
+    res.send({massage:'all good',product})
+})
+
+app.post('/store/create',(req,res)=>{
+    const data = req.body.data
+    product.push(data)
+    data? res.send('product Add') : res.send('error no product add')
+
+})
+app.listen(port, () => {
+  log(`this is the server:${port}`);
+});
