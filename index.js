4a93956a02ccaf659bdd3497cd2a71f48a85c639
DanielMalede <danielmalede@gmail.com>
2022-11-13 11:31:01 +0200

    .

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 const port = 5000;
 
 const getIndex = (req) => {
-  const findItem = product.find((item) => item.id == req.body.id);
-  const index = product.includes(findItem);
+  const findItem = product.find((item) => item.id == req.params.id);
+  const index = product.indexOf(findItem);
   return index;
 };
 
@@ -53,10 +53,23 @@ const product = [
     img: "someUrl",
   },
 ];
+
+const employees = [
+  {
+    id: 1,
+    name: "daniel",
+    age: 25,
+    phoneNumber: 0524755555,
+    email: "daniel@hmail.com",
+  },
+];
 app.use(cors());
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("My Store");
+});
 app.get("/store", (req, res) => {
   res.send("server is good");
 });
@@ -71,11 +84,23 @@ app.post("/store/create", (req, res) => {
   data ? res.send("product Add") : res.send("error no product add");
 });
 
-app.put("/store/edit", (req, res) => {
+app.put("/store/edit/:id", (req, res) => {
   const productIndex = getIndex(req);
   if (productIndex > -1) {
     product[productIndex] = req.body.data;
   }
+  res.send("successfully edit");
+});
+
+app.delete("/store/delete/:id", (req, res) => {
+  const indexProduct = getIndex(req);
+  const deleteProduct = product.splice(indexProduct, 1);
+  res.send(product);
+});
+
+app.get("/store/product/id/:id", (req, res) => {
+  const findProduct = product.find((item) => item.id == req.params.id);
+  res.send(findProduct);
 });
 
 app.listen(port, () => {
